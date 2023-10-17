8ec3b9db956d0983bdadca9258d393e69d0f0c60
kirby-b <kirby.brodie66@gmail.com>
2023-10-17 17:52:41 -0400

    Create and coding
    -made quicksort.py
    -Made all functions
    -Added comment at the top
    -Tested it to make sure it works
    -Added print to see end result

---
+++ Comparison-Sorts/quickSort.py
@@ -0,0 +1,48 @@
+def main():	
+    	# quick sort = moves smaller elements to left of a pivot.
+    	#			   recursively divide array in 2 partitions
+
+    	#                       run-time complexity = Best case O(n log(n))
+    	#				   		                   Average case O(n log(n))
+    	#				   		                   Worst case O(n^2) if already sorted
+    	
+    	#                       space complexity    = O(log(n)) due to recursion
+    	
+        intArray = [8, 2, 5, 3, 9, 4, 7, 6, 1]
+        
+        quickSort(intArray, 0, len(intArray) - 1)
+        
+        for i in intArray:
+            print(str(i) + " ")
+        
+
+def quickSort(sortingArray: list[int], start: int, end: int):
+		
+		if(end <= start): return #base case
+		
+		pivot = partition(sortingArray, start, end)
+		quickSort(sortingArray, start, pivot - 1)
+		quickSort(sortingArray, pivot + 1, end)		
+	
+def partition(sortingArray: list[int], start: int, end: int):
+		
+		pivot = sortingArray[end]
+		i = start - 1
+		j = start
+		while j <= end:
+			if sortingArray[j] < pivot:
+				i+= 1
+				temp = sortingArray[i]
+				sortingArray[i] = sortingArray[j]
+				sortingArray[j] = temp
+			j+= 1
+		
+		i+= 1
+		temp = sortingArray[i]
+		sortingArray[i] = sortingArray[end]
+		sortingArray[end] = temp
+		
+		return i
+	
+if __name__ == "__main__":
+    main()
