2006a3913c1fe20cdc2fc27e14d6ebbe236a6896
kirby-b <kirby.brodie66@gmail.com>
2024-01-12 06:40:28 -0500

    Create bubbleSort.py
    Made a bubble sort

---
+++ Comparison-Sorts/bubbleSort.py
@@ -0,0 +1,29 @@
+class BubbleSort():
+	
+	# bubble sort = pairs of adjacent elements are compared, and the elements
+	# 	            swapped if they are not in order.
+	
+	# Quadratic time O(n^2) so a small set is okay, but a big set is painful
+	
+	def main():
+		
+		nums = [9, 1, 8, 2, 7, 3, 6, 4, 5]
+		
+		BubbleSort.bubbleSort(nums)
+		
+		for x in range(len(nums)):
+			print(nums[x] , end="")
+	
+	def bubbleSort(array):
+		
+		for x in range(len(array) - 1):
+			for y in range(len(array)- x - 1):
+				if(array[y] > array[y+1]):
+					temp = array[y]
+					array[y] = array[y+1]
+					array[y+1] = temp
+				
+			
+if __name__ == "__main__":
+    BubbleSort.main()
+	
