f20a98bbc211c8a323872cb42100619cb31f4eef
kirby-b <kirby.brodie66@gmail.com>
2024-02-04 21:23:06 -0500

    Update quickSort.py

--- Comparison-Sorts/quickSort.py
+++ Comparison-Sorts/quickSort.py
@@ -8,39 +8,39 @@ def main():
     	
     	#                       space complexity    = O(log(n)) due to recursion
     	
-        intArray = [8, 2, 5, 3, 9, 4, 7, 6, 1]
+        int_array = [8, 2, 5, 3, 9, 4, 7, 6, 1]
         
-        quickSort(intArray, 0, len(intArray) - 1)
+        quick_sort(int_array, 0, len(int_array) - 1)
         
-        for i in intArray:
+        for i in int_array:
             print(str(i) + " ")
         
 
-def quickSort(sortingArray: list[int], start: int, end: int):
+def quick_sort(sorting_array: list[int], start: int, end: int):
 		
 		if(end <= start): return
 		
-		pivot = partition(sortingArray, start, end)
-		quickSort(sortingArray, start, pivot - 1)
-		quickSort(sortingArray, pivot + 1, end)		
+		pivot = partition(sorting_array, start, end)
+		quick_sort(sorting_array, start, pivot - 1)
+		quick_sort(sorting_array, pivot + 1, end)		
 	
-def partition(sortingArray: list[int], start: int, end: int):
+def partition(sorting_array: list[int], start: int, end: int):
 		
-		pivot = sortingArray[end]
+		pivot = sorting_array[end]
 		i = start - 1
 		j = start
 		while j <= end:
-			if sortingArray[j] < pivot:
+			if sorting_array[j] < pivot:
 				i+= 1
-				temp = sortingArray[i]
-				sortingArray[i] = sortingArray[j]
-				sortingArray[j] = temp
+				temp = sorting_array[i]
+				sorting_array[i] = sorting_array[j]
+				sorting_array[j] = temp
 			j+= 1
 		
 		i+= 1
-		temp = sortingArray[i]
-		sortingArray[i] = sortingArray[end]
-		sortingArray[end] = temp
+		temp = sorting_array[i]
+		sorting_array[i] = sorting_array[end]
+		sorting_array[end] = temp
 		
 		return i
 	
