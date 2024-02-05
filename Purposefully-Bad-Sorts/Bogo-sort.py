53c99428b247c920cc785c487b2df70633d70036
kirby-b <kirby.brodie66@gmail.com>
2024-02-04 21:23:08 -0500

    Update Bogo-sort.py

--- Purposefully-Bad-Sorts/Bogo-sort.py
+++ Purposefully-Bad-Sorts/Bogo-sort.py
@@ -2,14 +2,14 @@ import random
 
 def main():
     numbers = [1,3,5,4,9,6,8,7,2,0]
-    while(isSorted(numbers) == False):
+    while(is_sorted(numbers) == False):
         for x in range(len(numbers)):
             print(numbers[x])
         random.shuffle(numbers)
     #Randomizes the list item positions until the list is sorted
     print("Sorted!")
 
-def isSorted(nums):
+def is_sorted(nums):
     l = len(nums)
     for x in range(0, l-1):
         if nums[x] > nums[x + 1]:
