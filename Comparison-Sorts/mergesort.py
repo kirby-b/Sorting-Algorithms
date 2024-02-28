446c06cffac887d2b1daa47eb9c39f060b939a06
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 18:09:19 -0500

    Update mergesort.py

--- Comparison-Sorts/mergesort.py
+++ Comparison-Sorts/mergesort.py
@@ -1,69 +1,67 @@
-class MergeSort():
-    def main():  	
-        # merge sort = recursively divide nums array in 2, sort, re-combine
-        # run-time complexity = O(n Log n)
-        # space complexity    = O(n)
-        
-        nums = [8, 2, 5, 3, 4, 7, 6, 1]
-        
-        MergeSort.mergeSort(nums)
-        
-        for x in range(len(nums)):
-            print(nums[x], end=" ")
+def main():
+    # merge sort = recursively divide nums array in 2, sort, re-combine
+    # run-time complexity = O(n Log n)
+    # space complexity    = O(n)
+
+    nums = [8, 2, 5, 3, 4, 7, 6, 1]
+
+    merge_sort(nums)
+
+    for x in range(len(nums)):
+        print(nums[x], end=" ")
 
 
-    def mergeSort(nums):
-        
-        length = len(nums)
-        if (length <= 1): return nums#base case
-        
-        middle = length / 2
-        leftnums = []
-        rightnums = []
-        
-        i = 0 #left array
-        j = 0 #right array
-        
-        for i in range(length): 
-            if(i < middle):
-                leftnums.append(nums[i])
-            else:
-                rightnums.append(nums[i])
-                j+= 1
+def merge_sort(nums):
+    length = len(nums)
+    if length <= 1: return nums  # base case
 
-        MergeSort.mergeSort(leftnums)
-        MergeSort.mergeSort(rightnums)
-        MergeSort.merge(leftnums, rightnums, nums)
+    middle = length / 2
+    leftnums = []
+    rightnums = []
 
-    def merge(leftnums, rightnums, nums):
-        
-        leftSize = len(nums) / 2
-        rightSize = len(nums) - leftSize
-        i = 0
-        l = 0
-        r = 0 #indices
-        
-        #check the conditions for merging
-        while(l < leftSize and r < rightSize):
-            if(leftnums[l] < rightnums[r]):
-                nums[i] = leftnums[l]
-                i+= 1
-                l+= 1
-            else:
-                nums[i] = rightnums[r]
-                i+= 1
-                r+= 1
+    i = 0  # left array
+    j = 0  # right array
 
-        while(l < leftSize):
-            nums[i] = leftnums[l]
-            i+= 1
-            l+= 1
+    for i in range(length):
+        if i < middle:
+            leftnums.append(nums[i])
+        else:
+            rightnums.append(nums[i])
+            j += 1
 
-        while(r < rightSize):
-            nums[i] = rightnums[r]
-            i+= 1
-            r+= 1
+    merge_sort(leftnums)
+    merge_sort(rightnums)
+    merge(leftnums, rightnums, nums)
+
+
+def merge(leftnums, rightnums, nums):
+    left_size = len(nums) / 2
+    right_size = len(nums) - left_size
+    i = 0
+    left = 0
+    right = 0  # indices
+
+    # check the conditions for merging
+    while left < left_size and right < right_size:
+        if leftnums[left] < rightnums[right]:
+            nums[i] = leftnums[left]
+            i += 1
+            left += 1
+        else:
+            nums[i] = rightnums[right]
+            i += 1
+            right += 1
+
+    while left < left_size:
+        nums[i] = leftnums[left]
+        i += 1
+        left += 1
+
+    while right < right_size:
+        nums[i] = rightnums[right]
+        i += 1
+        right += 1
+
 
 if __name__ == "__main__":
-	MergeSort.main()
-	
+    main()
